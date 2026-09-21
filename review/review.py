fa27a01988177068246fb0f521dc45d2fc758e0d
brandon park <brandonnparkk@gmail.com>
2026-09-20 17:51:26 -0700

    update to add review list and add work on nearest_exist problem

---
+++ review/review.py
@@ -0,0 +1,3 @@
+# list of questions to review
+# nearest_exit: https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/description/?envType=study-plan-v2&envId=leetcode-75
+# rotten_oranges: https://leetcode.com/problems/rotting-oranges/description/
